0654871cb1f42b3dc2bb9f6f445012f4d6685cc1
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2024-03-12 15:25:56 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+/** 
+  * @author Reem Belafkih, Fatoumata Barry
+  * @version 1.20.2
+**/
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+public class Main{
+    public static void main(String[] args) throws FontFormatException, IOException
+    {
+        Game Game = new Game();
+    }
+}
